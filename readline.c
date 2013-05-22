a7cbbe07de83a98d0176209d4ae452436d2d48e3
acelight <curt.strangward@yahoo.co.uk>
2013-05-22 04:58:50 +0200

    Update to hopefully make it compatible with Lua 5.2

--- readline.c
+++ readline.c
@@ -1,37 +1,34 @@
 // build@ cc -shared readline.c -o readline.so
 // Completely and absolutely minimal binding to the readline library
+// 2011 Reuben Thomas <rrt@sc3d.org>
 // Steve Donovan, 2007
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdlib.h>
 #include "lua.h"
 #include "lauxlib.h"
-#include "lualib.h"
 #include <readline/readline.h>
 #include <readline/history.h>
 
-static int f_readline(lua_State* L)
-{
+static int f_readline (lua_State *L) {
     const char* prompt = lua_tostring(L,1);
-    const char* line = readline(prompt);
-    lua_pushstring(L,line);
-    (void)free((void *)line); // Lua makes a copy...
+    char* line = readline(prompt);
+    lua_pushstring(L, line);
+    free((char*)line); // Lua makes a copy...
     return 1;
-}
+};
 
-static int f_add_history(lua_State* L)
-{
-    if (lua_strlen(L,1) > 0)
-        add_history(lua_tostring(L, 1));
+static int f_add_history (lua_State *L) {
+    const char* line = lua_tostring(L,1);
+    add_history(line);
     return 0;
-}
+};
 
-static const struct luaL_reg lib[] = {
+static const luaL_Reg lib[] = {
 	{"readline", f_readline},
-    {"add_history",f_add_history},
+    {"add_history", f_add_history},
 	{NULL, NULL},
 };
 
 int luaopen_readline (lua_State *L) {
-	luaL_openlib (L, "readline", lib, 0);
+    luaL_newlib (L, lib);
 	return 1;
-}
+};
